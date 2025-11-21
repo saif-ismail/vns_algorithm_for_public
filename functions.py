b378065bdbbf7e2e1022465845b52b415c049110
Robin_Dunhuang <robin.vanderhaar@kuleuven.be>
2025-11-21 17:41:40 +0100

    Changed criterions to have determinant check in code itself rather than in criterion

--- functions.py
+++ functions.py
@@ -98,7 +98,7 @@ def filter_constraints(all_combs:pd.DataFrame, constraints:list):
     '''
     THIS FUNCTION FILTERS THE DATAFRAME BASED ON THE PROVIDED CONSTRAINTS
     '''    
-    if not constraints:
+    if not constraints or len(' & '.join(constraints)) == 0:
         return all_combs
 
     query_str = ' & '.join(constraints)
@@ -186,7 +186,7 @@ def get_start_design(candidate_set_expanded:np.ndarray, no_start_points:int, cos
             info_mat_i = full_mat.T @ full_mat
             det = np.linalg.det(info_mat_i)
             if det > 0:
-                return full_mat, current_cost_for_all_vars, evaluation_calculation(full_mat, np.inf)[1]
+                return full_mat, current_cost_for_all_vars, evaluation_calculation(info_mat_i, np.inf)[1]
             else:
                 continue
 
@@ -194,12 +194,12 @@ def get_start_design(candidate_set_expanded:np.ndarray, no_start_points:int, cos
 
 def evaluation_func(criterion:str):
     if criterion == 'D':
-        def calc(model_mat:np.ndarray, current_score:float):
-            val = np.linalg.det(model_mat.T @ model_mat)
+        def calc(info_mat:np.ndarray, current_score:float):
+            val = np.linalg.det(info_mat)
             return val > current_score, val
+        
     elif criterion == 'A':
-        def calc(model_mat:np.ndarray, current_score:float):
-            info_mat = model_mat.T @ model_mat
+        def calc(info_mat:np.ndarray, current_score:float):
             val = np.trace(np.linalg.inv(info_mat))
             return val < current_score, val
     return calc
@@ -237,7 +237,12 @@ def neigborhood_search(candidate_set_expanded:np.ndarray, start_design:np.ndarra
         if np.any(additional_cost > available_budget):
             continue
         tmp_full = np.r_[np.delete(start_design, list(row_to_drop), 0), candidate_set_expanded[list(row_to_add),:]]
-        quality_check, criterion_value = evaluation_calculation(tmp_full, current_best_criterion_value)
+        info_mat = tmp_full.T @ tmp_full
+        if np.linalg.det(info_mat) > 0:
+            quality_check, criterion_value = evaluation_calculation(info_mat, current_best_criterion_value)
+        else:
+            continue
+
         if quality_check:
             change_made = True
             removed_old_run_costs = np.delete(current_best_cost, row_to_drop, axis=0)
